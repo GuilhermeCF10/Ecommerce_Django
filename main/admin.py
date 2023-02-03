9e2cd84be965d1d8e8f66f8fa7cacdca5c6c3701
Guilherme Fialho <guilhermec.fialho@gmail.com>
2023-02-03 15:48:57 -0300

    Updating

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Categoria, Produto
+from .models import Categoria, Produto, Pedido, ItemPedido
 # Register your models here.
 
 @admin.register(Categoria)
@@ -13,3 +13,17 @@ class ProdutoAdmin(admin.ModelAdmin):
     list_filter = ['disponivel', 'data_criacao','data_ultima_atualizacao']
     list_editable = ['preco', 'estoque', 'disponivel']
     prepopulated_fields = {'slug': ('nome',)}
+
+
+class ItemPedidoInline(admin.TabularInline):
+    model = ItemPedido
+    raw_id_fields = ['produto']
+
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'email', 'logradouro', 'numero',
+    'complemento', 'bairro', 'cidade',
+    'uf', 'cep', 'data_criacao', 'pago']
+    list_filter = ['pago', 'data_criacao', 'nome']
+    inlines = [ItemPedidoInline]
