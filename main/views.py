19f29dd279cc76a23038ffe297bace35caa36b67
Guilherme Fialho <guilhermec.fialho@gmail.com>
2023-02-07 19:02:11 -0300

    Updating

--- main/views.py
+++ main/views.py
@@ -21,15 +21,6 @@ class ViewFaleConosco(FormView):
         form.enviar_mensagem_por_email()
         return super().form_valid(form)
     
-    # if request.method == 'POST':
-     #   meu_campo = rquest.POST.get('meu_campo')
-      #  formulario = forms.FormFaleConosco(request.POST)
-       # nome = formulario['nome'].value() # Primeira forma, acessando por chave
-       # nome = formulario.data['nome'] # Segunda forma, por chave
-#
- #       if formulario.is_valid():
-            #nome = formulario.cleaned_data['nome'] # Primeira forma
-            #nome = formulario.instance.nome # Segunda forma
 
 # ======================
 #   Listar Produtos
